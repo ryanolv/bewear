aa70e9b011d58564ebea0f957bc7e856cc401296
Ryan Oliveira <ryanprofissional02@gmail.com>
2026-01-20 20:16:30 -0300

    feat: expand database schema with categories, products, and product variants tables

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,37 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { boolean, integer, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
+export const categoriesTable = pgTable("categories", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  name: text().notNull(),
+  slug: text().notNull(),
+  description: text().notNull(),
+  image: text().notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
+
+export const productsTable = pgTable("products", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  name: text().notNull(),
+  
+  description: text().notNull(),
+  categoryId: uuid("category_id").references(() => categoriesTable.id),
+  isActive: boolean("is_active").notNull().default(true),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
+
+export const productVariantsTable = pgTable("product_variants", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  productId: uuid("product_id").references(() => productsTable.id),
+  name: text().notNull(),
+  description: text().notNull(),
+  priceInCents: integer().notNull(),
+  stock: integer().notNull(),
+  size: text().notNull(),
+  color: text().notNull(),
+  images: text("images").array().notNull(),
+  isActive: boolean("is_active").notNull().default(true),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
 });
